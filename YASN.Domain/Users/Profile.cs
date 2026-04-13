24f3c36a15931a6aed1a7b6a5f33dc8eae1d5c8b
Alexander <alexanderklush8@gmail.com>
2026-04-13 19:11:15 +0300

    refactor: true profile create time

--- YASN.Domain/Users/Profile.cs
+++ YASN.Domain/Users/Profile.cs
@@ -4,14 +4,14 @@ public class Profile
 {
     public Guid Id { get; private set; }
     public string Username { get; private set; }
-    public string Description { get; private set; }
+    public string? Description { get; private set; }
     public DateTime Created { get; private set; }
 
-    public Profile(Guid id, string username, string description, DateTime created)
+    public Profile(Guid id, string username, string? description = null)
     {
         Id = id;
         Username = username;
         Description = description;
-        Created = created;
+        Created = DateTime.UtcNow;
     }
 }
